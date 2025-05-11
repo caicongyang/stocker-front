66350b0a7ea2da66168910c0ce9ea5d8534deb5a
caicongyang <caicongyang@gmail.com>
2025-05-11 16:23:49 +0800

    feature: add some code

--- src/views/FinancialNews.vue
+++ src/views/FinancialNews.vue
@@ -158,7 +158,7 @@
           <p>如果此网站对您有帮助，欢迎打赏支持</p>
         </div>
         <div class="donation-qr">
-          <img src="/public/QRCode.jpg" alt="微信支付二维码" />
+          <img src="QRCode.jpg" alt="微信支付二维码" />
         </div>
         <div class="donation-footer">
           <p>感谢您的支持 ❤️</p>
